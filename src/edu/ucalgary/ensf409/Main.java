e580f413e7139a3234157eba42cc92e39756112a
Asad Anjum <anjumfamily6@gmail.com>
2021-04-02 14:51:05 -0600

    deleted nice greeting because i am meen

--- src/edu/ucalgary/ensf409/Main.java
+++ src/edu/ucalgary/ensf409/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String args[]) throws Exception {
         OrderUserInterface order = new OrderUserInterface();
         order.selectFurnitureCategory();
-        System.out.println("Hello");
     }
 }
